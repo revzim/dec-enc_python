4518455d611b58187eaa77f3d0578fdcf0093244
andyzim <andyzim11@gmail.com>
2015-08-24 21:56:11 -0400

    added u_p.txt and user choice

--- decrypt_encrypt.py
+++ decrypt_encrypt.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import AES
+import hashlib
 import base64
 import os
 import getpass
@@ -9,6 +10,11 @@ padding = '{'
 
 pad = lambda s: s + (blk_size - len(s) % blk_size) * padding
 
+USER = getpass.getuser()
+#hashlib.md5('goingbacktocali').hexdigest()
+PW_HASH = '6f31f0f01e9c37613ce249db9dcf700a'
+
+
 encode_aes = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
 decode_aes = lambda c, e: c.decrypt(base64.b64decode(e)).strip(padding)
 
@@ -16,30 +22,66 @@ secret = os.urandom(blk_size)
 
 cipher = AES.new(secret)
 
-print 'Please Input your message:',
-user_input = raw_input()
+user = {
+    'Username': '',
+    'Password': ''
+    }
 
-encoded = encode_aes(cipher, user_input)
-print 'You\'re encrypted message:', encoded
+def get_username_and_pw(u, p):
+    print 'Please enter your username'
+    u = raw_input()
+    print 'Please enter your password'
+    p = raw_input()
+    user['Username'] = u
+    user['Password'] = p
+    return user
 
-#decode
-decoded = decode_aes(cipher, encoded)
-#print 'Decrypted string:', decoded
+get_username_and_pw(user['Username'], user['Password'])
+pwfile = open('usernames_passwords.txt', 'w+')
+pwfile.read()
 
-print 'Do you want to decrypt your message?'
-user_in = raw_input()
+for line in pwfile:
+    if line.__contains__(user):
+        print 'Hello, %s welcome!' % (user['Username'])
+    else:
+        print 'Welcome, first timer, %s!' %s (user['Username'])
+        pwfile.write('%s' % user)
+        pwfile.close()
 
-user_in.lower()
-
-if user_in == 'yes':
-    print 'Please enter your username:'
-    uname = raw_input()
-    if uname == getpass.getuser():
-        file = open('messages.txt', "w+")
-        file.write('The message from %s: %s\n' % (uname, decoded))
-        file.close()
-        print "The file was written to messages.txt, please look to find your message decoded"
-else:
-    print 'Ok, have a good day!'
-
-
+def options_for_user():
+    read_message = 'read messages'
+    add_message = 'add message'
+    delete_message = 'delete messages'
+    print 'Hello %s, what would you like to do?' % (user['Username'])
+    print 'Your choices are: %s, %s, or %s' % (read_message, add_message, delete_message)
+    choice = raw_input()
+    choice.lower()
+    message_file = open('messages.txt', 'r')
+    if choice == read_message:
+        print 'Opening messages file...'
+        print message_file.read()
+    elif choice == add_message:
+        print 'What is the message you would like to add?'
+        user_input = raw_input()
+        encoded = encode_aes(cipher, user_input)
+        decoded = decode_aes(cipher, encoded)
+        print 'Would you like the message to be added encoded or decoded?'
+        choice_2 = raw_input()
+        if choice_2 == 'encoded':
+            print "writing encoded message to messages.txt"
+            f = open('messages.txt', "r+")
+            f.write('The message from %s (encoded): %s\n' %(user['Username'], encoded))
+            f.close()
+        else:
+            print 'writing decoded message to messages.txt'
+            f = open('messages.txt', 'r+')
+            f.write('The message from %s (decoded): %s\n' %(user['Username'], decoded))
+            f.close()
+    elif choice == delete_message:
+        print 'Deleting messages from messages.txt'
+        f = open('messages.txt', 'r+')
+        f.truncate()
+    else:
+        print "Please either enter 'read messages', 'add message', or 'delete messages'"
+        options_for_user()
+options_for_user()
